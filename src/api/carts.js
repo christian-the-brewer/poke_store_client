a174f8361c7cfa539ab8f20ffee150f3116d48cf
christian-the-brewer <brewer.christiang@gmail.com>
2022-08-05 11:19:50 -0400

    Adds carts.js in api and a Show cart component

---
+++ src/api/carts.js
@@ -0,0 +1,54 @@
+import apiUrl from '../apiConfig'
+import axios from 'axios'
+
+//READ => INDEX
+export const getAllCarts = () => {
+    return axios({
+        url: `${apiUrl}/carts`,
+        method: 'GET'
+    })
+}
+
+// READ => SHOW
+export const getOneCart = (id) => {
+    return axios(`${apiUrl}/carts/${id}`)
+}
+
+// CREATE
+export const createCart = (user, newCart) => {
+
+    return axios({
+        url: apiUrl + '/carts',
+        method: 'POST',
+        headers: {
+            Authorization: `Token token=${user.token}`,
+        },
+        data: { cart: newCart }
+    })
+}
+
+// UPDATE
+export const updateCart = (user, updatedCart) => {
+
+    console.log('this is updatedCart', updatedCart)
+
+    return axios({
+        url: `${apiUrl}/carts/${updatedCart._id}`,
+        method: 'PATCH',
+        headers: {
+            Authorization: `Token token=${user.token}`,
+        },
+        data: { cart: updatedCart }
+    })
+}
+
+// DELETE
+export const removeCart = (user, cartId) => {
+    return axios({
+        url: `${apiUrl}/carts/${cartId}`,
+        method: 'DELETE',
+        headers: {
+            Authorization: `Token token=${user.token}`,
+        }
+    })
+}
---
+++ src/components/cart/ShowCart.js
@@ -0,0 +1,95 @@
+import { useState, useEffect } from 'react'
+
+import { useParams, useNavigate } from 'react-router-dom'
+
+import { Container, Card, Button } from 'react-bootstrap'
+
+import LoadingScreen from '../shared/LoadingScreen'
+import { getOneCart, updateCart } from '../../api/carts'
+import messages from '../shared/AutoDismissAlert/messages'
+import EditCartModal from './EditCartModal'
+
+
+const ShowItem = (props) => {
+    const [cart, setCart] = useState(null)
+    const [editModalShow, setEditModalShow] = useState(false)
+    const [updated, setUpdated] = useState(false)
+
+    const { id } = useParams()
+    const navigate = useNavigate()
+
+    const { user, msgAlert } = props
+    console.log('user in props', user)
+    console.log('the cart in showCart', cart)
+
+
+    useEffect(() => {
+        getOneCart(id)
+            .then(res => setCart(res.data.cart))
+            .catch(err => {
+                msgAlert({
+                    heading: 'Error getting cart',
+                    message: messages.getCartsFailure,
+                    variant: 'danger'
+                })
+                navigate('/')
+            })
+    }, [updated])
+
+
+
+    if (!cart) {
+        return <LoadingScreen />
+    }
+
+
+    const cartProducts = cart.map((product, index) => (
+        <li key={index}>{product.name}: {product.cost}</li>
+    ))
+
+    return (
+        <>
+            <Container className="fluid">
+                <Card>
+                    <Card.Header>Order</Card.Header>
+                    <Card.Body>
+
+                        <Card.Text>
+                            <ul>
+                                {cartProducts}
+                            </ul>
+
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        {
+
+                            <>
+                                <Button onClick={() => setEditModalShow(true)}
+                                    className="m-2"
+                                    variant="warning"
+                                >
+                                    Edit Cart
+                                </Button>
+
+                            </>
+
+
+                        }
+                    </Card.Footer>
+                </Card>
+            </Container>
+            <EditCartModal
+                user={user}
+                cart={cart}
+                show={editModalShow}
+                updateCart={updateCart}
+                msgAlert={msgAlert}
+                triggerRefresh={() => setUpdated(prev => !prev)}
+                handleClose={() => setEditModalShow(false)}
+            />
+        </>
+    )
+}
+
+export default ShowCart
